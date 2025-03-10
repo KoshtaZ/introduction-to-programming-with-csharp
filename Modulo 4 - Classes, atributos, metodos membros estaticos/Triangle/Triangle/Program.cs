7044877a6991837e14cc54a3d84c500fec9ee1b7
José Costa <josecosta@Mac.Home>
2025-03-10 16:34:38 +0000

    Triangle Area - version1

---
+++ Modulo 4 - Classes, atributos, metodos membros estaticos/Triangle/Triangle/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double xA, xB, xC, yA, yB, yC;
+            
+            Console.WriteLine("Enter X coordinate: ");
+            xA = double.Parse(Console.ReadLine());
+            xB = double.Parse(Console.ReadLine());
+            xC = double.Parse(Console.ReadLine());
+            
+            
+            Console.WriteLine("Enter Y coordinate: ");
+            yA = double.Parse(Console.ReadLine());
+            yB = double.Parse(Console.ReadLine());
+            yC = double.Parse(Console.ReadLine());
+
+        }
+    }
+}
+
