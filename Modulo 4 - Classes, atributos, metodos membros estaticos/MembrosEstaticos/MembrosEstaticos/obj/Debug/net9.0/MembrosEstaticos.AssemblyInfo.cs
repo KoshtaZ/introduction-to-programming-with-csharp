c582933c604c3419b71276c2d81c6a8c10377884
José Costa <josecosta@Mac.Home>
2025-03-19 21:33:10 +0000

    Membros estaticos versão 2

--- Modulo 4 - Classes, atributos, metodos membros estaticos/MembrosEstaticos/MembrosEstaticos/obj/Debug/net9.0/MembrosEstaticos.AssemblyInfo.cs
+++ Modulo 4 - Classes, atributos, metodos membros estaticos/MembrosEstaticos/MembrosEstaticos/obj/Debug/net9.0/MembrosEstaticos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MembrosEstaticos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92b6e5ed104d017552ed2e3c1b5f82787eb0e09a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9de749d8ca29b033132744e57c39a92152ed7fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MembrosEstaticos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MembrosEstaticos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
